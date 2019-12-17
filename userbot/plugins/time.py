2b80383b87ac20d2603ac361a8bd2e0c1b51428b
japarmar <55135328+japarmar@users.noreply.github.com>
2019-12-17 21:29:54 +0530

    Update time.py

--- userbot/plugins/time.py
+++ userbot/plugins/time.py
@@ -36,7 +36,7 @@ async def _(event):
     await borg.send_file(  # pylint:disable=E0602
         event.chat_id,
         required_file_name,
-        caption="Userbot: Powered by @x-tra-telegram",
+        caption="Userbot: Powered by @Halto_Tha",
         # Courtesy: @ManueI15
         reply_to=reply_msg_id
     )
