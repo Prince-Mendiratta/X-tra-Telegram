7c6b1d5a5915f5d1a637881efd0c47450335a78e
japarmar <55135328+japarmar@users.noreply.github.com>
2020-01-03 17:30:12 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -9,7 +9,7 @@ PM_WARNS = {}
 PREV_REPLY_MESSAGE = {}
 
 
-USER_BOT_WARN_ZERO = "I am currently offline. Please do not SPAM me.Else gaand mara dunga bsdk "
+USER_BOT_WARN_ZERO = "I am currently offline. Please do not SPAM me. "
 USER_BOT_NO_WARN = "`[──▄█▀█▄─────────██ \n▄████████▄───▄▀█▄▄▄▄ \n██▀▼▼▼▼▼─▄▀──█▄▄ \n█████▄▲▲▲─▄▄▄▀───▀▄ \n██████▀▀▀▀─▀────────▀▀]\n\nHello, I'm Personal Assistance of Jayu Sir. If you spam you will be reported and blocked, so make sure you **DON'T SPAM**.\n\nYour companionship is always an opportunity But Sed This line is not for you.`"
 
 if Var.PRIVATE_GROUP_ID is not None:
