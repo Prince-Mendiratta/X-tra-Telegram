bccf48adc074f0150886a1ba634d674aac381d02
japarmar <55135328+japarmar@users.noreply.github.com>
2020-01-03 17:26:12 +0530

    Update mf.py

--- userbot/plugins/mf.py
+++ userbot/plugins/mf.py
@@ -3,7 +3,7 @@ from telethon import events, functions, __version__
 from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="mf ?(.*)", allow_sudo=True))  # pylint:disable=E0602
+@borg.on(admin_cmd(pattern="mf", allow_sudo=True))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
