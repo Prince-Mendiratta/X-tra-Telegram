f16d9dc57b93c56f0a1307a1c2f3a3e31db5cdfd
xditya <58950863+xditya@users.noreply.github.com>
2020-02-13 09:36:56 +0530

    __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -49,7 +49,7 @@ for name in files:
 
 import userbot._core
 
-print("Yay your userbot is officially working. Ja gaand mara")
+print("Yay your userbot is officially working. Thank @xditya 😘")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
