b1dcf200764b1b5cc5f8b8d492be25c1a6be6c4b
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-03-12 00:52:56 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -41,6 +41,22 @@ if Var.PRIVATE_GROUP_ID is not None:
                 await event.delete()
 
 
+    @bot.on(events.NewMessage(outgoing=True))
+    async def you_dm_niqq(event):
+        if event.fwd_from:
+            return
+        replied_user = await event.client(GetFullUserRequest(event.chat_id))
+        firstname = replied_user.user.first_name
+        chat = await event.get_chat()
+        if event.is_private:
+            if not pmpermit_sql.is_approved(chat.id):
+                if not chat.id in PM_WARNS:
+                    pmpermit_sql.approve(chat.id, reason)
+                    bruh = "Approved Nibba [{}](tg://user?id={})".format(firstname, chat.id)
+                    await borg.send_message(event.chat_id, bruh)
+                    await event.delete()
+
+
     @command(pattern="^.block ?(.*)")
     async def approve_p_m(event):
         if event.fwd_from:
