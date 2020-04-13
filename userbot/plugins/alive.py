0fe7b35ed7d0b7a7469339ea967cf469477b555d
harith9899 <63388123+harith9899@users.noreply.github.com>
2020-04-13 16:04:19 +0300

    Update alive.py
    """Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
    import asyncio
    from telethon import events
    from telethon.tl.types import ChannelParticipantsAdmins
    from platform import uname
    from userbot import ALIVE_NAME
    from userbot.utils import admin_cmd
    
    DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @PPP_Q"
    
    @command(outgoing=True, pattern="^.alive$")
    async def amireallyalive(alive):
        """ For .alive command, check if the bot is running.  """
        await alive.edit("**YO BRO JABTAK YE KHEL KHATAM NHI HOTA APUN IDHARICH HAI ψ(｀∇´)ψ**\n\n"
                         "Telethon version: 6.9.0\nPython: 3.7.3\nfork by:@CcCYo\n"
                         "Bot created by: [『ݲــٌـاࢪٽ آࢦـ نـَـࢩڪـل』](tg://user?id=992305036)\n"
                         "Database Status: Databases functioning normally!\n\nAlways with you, my master!\n"
                         f"My peru owner: {DEFAULTUSER}\n"
                         "[لصنع بوت راسلني](https://t.me/CcCYo)")

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,14 +6,14 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @XtraTgBot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @PPP_Q"
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("**`YO BRO JABTAK YE KHEL KHATAM NHI HOTA APUN IDHARICH HAI ψ(｀∇´)ψ`**\n\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @A_Dark_Princ3\n"
-                     "`Bot created by:` [SnapDragon](tg://user?id=719877937)\n"
+                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @CcCYo\n"
+                     "`Bot created by:` [SnapDragon](tg://user?id=992305036)\n"
                      "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
-                     f"`My peru owner`: {DEFAULTUSER}\n"
+                     f"`My peru owner`: {『ݲــٌـاࢪٽ آࢦـ نـَـࢩڪـل』}\n"
                      "[Deploy this userbot Now](https://github.com/Dark-Princ3/X-tra-Telegram)")
