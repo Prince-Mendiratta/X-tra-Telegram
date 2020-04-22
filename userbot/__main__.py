e21784a3ae367e16d50cf45cd241cf1a50c6f328
SHAHANUR  ALI AHMED <35138607+shahanurrhmn@users.noreply.github.com>
2020-04-22 11:55:23 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -49,7 +49,7 @@ for name in files:
 
 import userbot._core
 
-print("Yay your userbot is officially working. Ja gaand mara")
+print("Yep, Your userbot is officially working. Join @XtraTgBot for help")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
