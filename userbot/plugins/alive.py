faa94401ad8ce4fd584474389dd434a9b28e69ff
azofficial <62930663+azofficial@users.noreply.github.com>
2020-06-10 02:54:01 +0530

    alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,7 +11,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check p
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`Re Abhi Zinda Hu Malik!` **ψ(｀∇´)ψ**\n"
+    await alive.edit("`Are Abhi Zinda Hu Malik!` **ψ(｀∇´)ψ**\n"
                      "`Telethon version: 6.9.0\nPython: 3.7.3\n"
                      "`Bot created by:` [SnapDragon] @anubisxx\n"
                      f"`My peru owner`: {DEFAULTUSER}\n"
