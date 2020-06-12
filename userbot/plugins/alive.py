b44e39fc04c6bca0cef37a7debe1447807e435ac
azofficial <62930663+azofficial@users.noreply.github.com>
2020-06-12 14:53:02 +0530

    Defult alive

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,9 +11,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**No Name set yet.** [Check Gu
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`Are Abhi Zindahu malik , aapki kiya seva karsakta hu boliye!` **ψ(｀∇´)ψ**\n\n"
+    await alive.edit("`Are Abhi Apun Zanda He Malik, Kiya seva kar sakta hu!` **ψ(｀∇´)ψ**\n\n"
                      "`Telethon version: 6.9.0\nPython: 3.7.3\n`"
                      # Don't change this else you a TikTok loser, Son of Jinping. Add your own.
-                     "`Bot created by:` [SnapDragon], @anubisxx\n"
+                     "`Bot created by:` [SnapDragon](tg://user?id=719877937), @anubisxx\n"
                      f"`My peru owner`: {DEFAULTUSER}\n\n"
-                     
+                     "https://github.com/Dark-Princ3/X-tra-Telegram")
