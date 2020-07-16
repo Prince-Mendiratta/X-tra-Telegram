eefe9d0a832d23e9d533624b6349f52f66b28908
saskeuday <67370201+saskeuday@users.noreply.github.com>
2020-07-16 04:43:30 +0300

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,9 +11,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**No Name set yet.** [Check Gu
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`Currently Alive, my peru master!` **ψ(｀∇´)ψ**\n\n"
+    await alive.edit("`Currently Alive, my Assal master!` **ᓚᘏᗢ**\n\n"
                      "`Telethon version: 6.9.0\nPython: 3.7.3\n`"
                      # Don't change this else you a TikTok loser, Son of Jinping. Add your own.
-                     "`Bot created by:` [SnapDragon](tg://user?id=719877937), @anubisxx\n"
+                     "`Bot created by:` [𝒉𝒐𝒏𝒆𝒚](tg://user?id=621788749), @SASKE_89\n"
                      f"`My peru owner`: {DEFAULTUSER}\n\n"
-                     "https://github.com/Dark-Princ3/X-tra-Telegram")
+                     "**𝙸 𝙻𝙾𝚅𝙴 𝚈𝙾𝙺 𝙷𝙾𝙽𝙴𝚈 🍯🍓**")
