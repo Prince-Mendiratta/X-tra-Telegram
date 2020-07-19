5529b03b2d5d3fce28058f5e4a9e7c5a034df8e7
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-19 15:26:17 +0530

    Update getmusic.py

--- userbot/plugins/getmusic.py
+++ userbot/plugins/getmusic.py
@@ -1,50 +1,64 @@
-
+""" 
+created by @mrconfused
+syntax :- .song song name 
+     
+"""
+import os
+from bs4 import BeautifulSoup
+import requests
 from telethon import events
 import subprocess
 from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
 import io
 import asyncio
-import time
 from userbot.utils import admin_cmd
 import glob
-import os
-try:
- import instantmusic , subprocess
-except:
- os.system("pip install instantmusic")
- 
+import os  
 
-
-os.system("rm -rf *.mp3")
-
-
-def bruh(name):
-    
-    os.system("instantmusic -q -s "+name)
-    
-
-@borg.on(admin_cmd(pattern="song ?(.*)"))
+@borg.on(admin_cmd(pattern="song(?: |$)(.*)"))
 async def _(event):
-    if event.fwd_from:
-        return
-    DELAY_BETWEEN_EDITS = 0.3
-    PROCESS_RUN_TIME = 100
-    cmd = event.pattern_match.group(1)
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-    await event.edit("ok finding the song")    
-    bruh(str(cmd))
+    reply = await event.get_reply_message()
+    if event.pattern_match.group(1):
+        query = event.pattern_match.group(1)
+        await event.edit("wi8..! I am finding your song....")
+    elif reply.message:
+        query = reply.message
+        await event.edit("wi8..! I am finding your song....")
+    else:
+    	await event.edit("`What I am Supposed to find `")
+    	return
+    await catmusic(str(query),"128k")
     l = glob.glob("*.mp3")
-    loa = l[0]
-    await event.edit("sending song")
+    if l:
+        await event.edit("yeah..! i found something wi8..🥰")
+    else:
+        await event.edit(f"Sorry..! i can't find anything with `{query}`")
+    loa = l[0]    
     await borg.send_file(
                 event.chat_id,
                 loa,
                 force_document=True,
                 allow_cache=False,
-                caption=cmd,
+                caption=query,
                 reply_to=reply_to_id
             )
+    await event.delete()
     os.system("rm -rf *.mp3")
     subprocess.check_output("rm -rf *.mp3",shell=True)
+    
+async def catmusic(cat , QUALITY):
+  search = cat
+  headers = {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
+  html = requests.get('https://www.youtube.com/results?search_query='+search, headers=headers).text
+  soup = BeautifulSoup(html, 'html.parser')
+  for link in soup.find_all('a'):
+    if '/watch?v=' in link.get('href'):
+        # May change when Youtube Website may get updated in the future.
+        video_link = link.get('href') 
+        break
+  video_link =  'http://www.youtube.com/'+video_link
+  command = ('youtube-dl --extract-audio --audio-format mp3 --audio-quality ' + QUALITY + ' ' + video_link)	
+  os.system(command)    
